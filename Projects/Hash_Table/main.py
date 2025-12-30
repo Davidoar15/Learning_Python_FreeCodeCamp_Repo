9db419a8fd83e0d9cc035f30de868934376b5325
David Olivo Alfonso Rodríguez <dolivo@frba.utn.edu.ar>
2025-12-29 23:14:54 -0300

    Create main.py

---
+++ Projects/Hash_Table/main.py
@@ -0,0 +1,33 @@
+class HashTable:
+    def __init__(self):
+        self.collection = {}
+    
+    def hash(self, string):
+        return sum(ord(char) for char in string)
+
+    def add(self, key, value):
+        hashed = self.hash(key)
+
+        if hashed not in self.collection:
+            self.collection[hashed] = {}
+
+        self.collection[hashed][key] = value
+
+
+    def remove(self, key):
+        hashed = self.hash(key)
+
+        if hashed in self.collection:
+            if key in self.collection[hashed]:
+                del self.collection[hashed][key]
+
+            if not self.collection[hashed]:
+                return
+
+    def lookup(self, key):
+        hashed = self.hash(key)
+
+        if hashed in self.collection:
+            return self.collection[hashed][key]
+
+        return None
